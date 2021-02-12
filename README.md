c43434d3a6da8aeb4d672af267fb94fffa76b36e
Soham <iam.soham@gmail.com>
2021-02-12 15:32:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# liquibase-h2
+ Spring Boot + Liquibase + H2
---
+++ src/main/java/soham/spring/liquibaseh2/LiquibaseH2Application.java
@@ -0,0 +1,13 @@
+package soham.spring.liquibaseh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseH2Application.class, args);
+	}
+
+}
